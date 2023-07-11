f87cb7ff842438fff25db2abc0435ff51dbfcdd5
Missfara <faluyi94@gmail.com>
2023-07-11 09:52:17 +0100

    my first scissors project

--- src/firebase/services.tsx
+++ src/firebase/services.tsx
@@ -1,26 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth'
-
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAYEFmqPInSsWii_SyN_DFtPiz4yde40iA",
-    authDomain: "scissors-f287d.firebaseapp.com",
-    databaseURL: "https://scissors-f287d-default-rtdb.firebaseio.com",
-    projectId: "scissors-f287d",
-    storageBucket: "scissors-f287d.appspot.com",
-    messagingSenderId: "401646158793",
-    appId: "1:401646158793:web:895948d86f70ccb935de18",
-    measurementId: "G-JG2VTPV249"
+  apiKey: "AIzaSyCWwARD5lbtbLvizbEl-oj8GAl76NlYrn4",
+  authDomain: "myscissorproject.firebaseapp.com",
+  projectId: "myscissorproject",
+  storageBucket: "myscissorproject.appspot.com",
+  messagingSenderId: "700809117787",
+  appId: "1:700809117787:web:8ecaa0d484dd82c9fe3f7e",
+  measurementId: "G-2X8RE4F8WS",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -28,11 +25,9 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 
-
-
 export default getFirestore();
 
 //Use getAuth() to get the Auth service:
-export const auth = getAuth(app)
+export const auth = getAuth(app);
 
 // const analytics = getAnalytics(app);
